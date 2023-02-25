d0a8661fd3930e38c9613899a2f10e7bdfc1eaaf
DDube16 <dkgomo00@gmail.com>
2023-02-24 16:06:08 -0800

    update

---
+++ 0x02-functions_nested_loops/5-main.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+* main - check the code.
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+print_line(98);
+_putchar(',');
+_putchar(' ');
+_putchar('0');
+_putchar('\n');
+print_line(0);
+_putchar(',');
+_putchar(' ');
+_putchar('0');
+_putchar('\n');
+print_line(0xff);
+_putchar(',');
+_putchar(' ');
+_putchar('0');
+_putchar('\n');
+print_line(-1);
+_putchar(',');
+_putchar(' ');
+_putchar('0');
+_putchar('\n');
+return (0);
+}
+
---
+++ 0x02-functions_nested_loops/7-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+_putchar('0');
+_putchar('\n');
+return (0);
+}
